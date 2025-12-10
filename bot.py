69e1c4774cc3033393c1e15c15f34cb1e57e7f33
sokolalesia06-rgb <sokolalesia06@gmail.com>
2025-12-10 22:58:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import os
-from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -9,14 +7,13 @@ from telegram.ext import (
     filters
 )
 
-load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "ТУТ_ТВІЙ_ТОКЕН"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привіт! Бот працює 😊")
+    await update.message.reply_text("Привіт! Я працюю 🤖")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Ти написав: {update.message.text}")
+    await update.message.reply_text(update.message.text)
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
@@ -24,7 +21,6 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    print("Бот запущений!")
     app.run_polling()
 
 if __name__ == "__main__":
